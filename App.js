d97b2a4ae37151426e221f8f07cf2061f61af98a
vidhee1411 <vidhee@email.sc.edu>
2022-10-22 10:41:11 -0400

    New project setup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import AppNavigation from './src/Navigation/AppNavigation' 
+
 import SignInDoneScreen from './src/screens/SignInDoneScreen';
 import SignInScreen from './src/screens/SignInScreen';
 
--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/research/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/campuseats/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
