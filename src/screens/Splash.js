8957398c0cec2252b77d69587d31cbd7375b9eca
Siona Ravi <aravi@email.sc.edu>
2022-10-24 12:30:34 -0400

    updated splash screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -10,6 +10,8 @@ export default class Splash extends screens {
                 source={require('./images/logo.png')}
                 />
 
+                <Text>Welcome</Text>
+
                 
             </View>
         )
