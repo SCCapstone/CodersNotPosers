283dcd298bbd41a834020d0819a9b190cc2d6681
Siona Ravi <aravi@email.sc.edu>
2022-11-05 16:21:42 -0400

    updated sign in screen

--- App.js
+++ App.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 import SignInDoneScreen from './src/screens/SignInDoneScreen';
 import SignInScreen from './src/screens/SignInScreen';
-import Splash from './src/screens/Splash';
-
 
 export default function App() {
     return (
-            <Splash/>,
             <SignInScreen/>
     );
 }
