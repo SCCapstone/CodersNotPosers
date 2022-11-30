f5b835ff857436bfb96ca420560a8b87c5b30174
Vidhee1411 <58293207+Vidhee1411@users.noreply.github.com>
2022-11-30 14:51:21 -0500

    Revert "onboarding"
    This reverts commit d56405fc7bf67e0afe9a1a24958443a8c36a9eaa.

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import *  as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import OnboardingScreen from './src/screens/OnboardingScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
-
 const App = () => {
     return (
         <NavigationContainer>{
             <Stack.Navigator
-                headerMode="none"
+                screenOptions={{
+                    headerShown: false
+                }}
             >
-                <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-                <Stack.Screen name="SignIn" component={SignInScreen} />
-                <Stack.Screen name="SignUp" component={SignUpScreen} />
-            </Stack.Navigator>
+                    <Stack.Screen name="SignIn" component={SignInScreen} />
+                    <Stack.Screen name="SignUp" component={SignUpScreen} />
+                </Stack.Navigator>
         }</NavigationContainer>
     );
-}
+};
 export default App;
---
+++ src/screens/SignInDoneScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignInDoneScreen = ({navigation}) =>  {
+    return (
+        <View style={{alignItems:'center'}}>
+        <Text>
+            You are Signed In 
+        </Text>
+        </View>
+    );
+ }
+ export default SignInDoneScreen
