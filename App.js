48516d56c17713bff8dd377c10ca2b002d7de84f
jackhempstead <jackhemp01@gmail.com>
2022-11-30 21:50:40 -0500

    Server for Stripe attempt

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import paymentScreen from './src/screens/paymentScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -14,11 +15,17 @@ const App = () => {
                     headerShown: false
                 }}
             >
+                    {/* /*
+                   <StripeProvider
+                     publishableKey="pk_test_51M9ueME2kyK6I5WPJO8bYnghkiEiicrytMc9587Yr"
+                    </StripeProvider>
+            */ }
+
                     <Stack.Screen name="Payment" component={paymentScreen}/>
                     <Stack.Screen name="SignIn" component={SignInScreen} />
                     <Stack.Screen name="SignUp" component={SignUpScreen} />
                 </Stack.Navigator>
-        }</NavigationContainer>
+        }</NavigationContainer> 
     );
 };
 export default App;
