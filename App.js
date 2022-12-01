7593d96d6333bb5358901a214f9413e15be59f5d
jackhempstead <jackhemp01@gmail.com>
2022-11-30 22:02:21 -0500

    fixing upperCases

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import *  as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
-import paymentScreen from './src/screens/paymentScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+import RestaurantScreen from './src/screens/RestaurantScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StripeProvider } from '@stripe/stripe-react-native';
 
@@ -20,8 +21,8 @@ const App = () => {
                      publishableKey="pk_test_51M9ueME2kyK6I5WPJO8bYnghkiEiicrytMc9587Yr"
                     </StripeProvider>
             */ }
-
-                    <Stack.Screen name="Payment" component={paymentScreen}/>
+                    <Stack.Screen name="Restauraunts" component={RestaurantScreen}/>
+                    <Stack.Screen name="Payment" component={PaymentScreen}/>
                     <Stack.Screen name="SignIn" component={SignInScreen} />
                     <Stack.Screen name="SignUp" component={SignUpScreen} />
                 </Stack.Navigator>
