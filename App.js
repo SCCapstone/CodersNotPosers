19c368c92f66229cf46a6ec427f78679ec17391f
Nicholas Garofalo <Garofaln@email.sc.edu>
2022-12-01 20:13:20 -0500

    updates

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
-import RestaurantScreen from './src/screens/RestaurantScreen';
+import CampusSideSelectionScreen from './src/screens/CampusSideSelectionScreen';
+import RussellHouseRestaurantScreen from './src/screens/RussellHouseRestaurantScreen';
+import ChickfilAPage from './src/screens/ChickfilAPage';
+import DriverSignUpScreen from './src/screens/DriverSignUpScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StripeProvider } from '@stripe/stripe-react-native';
 
@@ -21,7 +24,10 @@ const App = () => {
                      publishableKey="pk_test_51M9ueME2kyK6I5WPJO8bYnghkiEiicrytMc9587Yr"
                     </StripeProvider>
             */ }
-                    <Stack.Screen name="Restauraunts" component={RestaurantScreen}/>
+                    <Stack.Screen name="CampusSelect" component={CampusSideSelectionScreen}/>
+                    <Stack.Screen name="RussellHouse" component={RussellHouseRestaurantScreen}/>
+                    <Stack.Screen name="CFA" component={ChickfilAPage}/>
+                    <Stack.Screen name="DriverSignUp" component={DriverSignUpScreen}/>
                     <Stack.Screen name="Payment" component={PaymentScreen}/>
                     <Stack.Screen name="SignIn" component={SignInScreen} />
                     <Stack.Screen name="SignUp" component={SignUpScreen} />
