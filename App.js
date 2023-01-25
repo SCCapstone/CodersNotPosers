388456d1ffa1122d37ffb4e314b1735f162809e5
Vidhee Patel <VIDHEE@email.sc.edu>
2023-01-25 13:52:07 -0500

    drawer file

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import OnboardingScreen from './src/screens/OnboardingScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
-import PaymentScreen from './src/screens/PaymentScreen';
+import PaymentScreen from './src/screens/paymentScreen';
 import CampusSideSelectionScreen from './src/screens/CampusSideSelectionScreen';
 import RussellHouseRestaurantScreen from './src/screens/RussellHouseRestaurantScreen';
 import ChickfilAPage from './src/screens/ChickfilAPage';
