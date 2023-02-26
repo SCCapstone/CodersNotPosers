01efa65a7395d689b01f3559f79a7386b375411c
Vidhee Patel <VIDHEE@email.sc.edu>
2023-02-25 22:33:08 -0500

    signout feature

--- src/screens/SignOutScreen.js
+++ src/screens/SignOutScreen.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert, Button,StyleSheet } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import SignInScreen from './SignInScreen';
+import DrawerNavigation from '../../navigation/DrawerNavigation';
 
-const Profile = () => {
+export default function SignOutScreen({navigation}) {
+  async function signOut() {
+    try {
+      await auth().signOut();
+      console.log("User successfully signed out");
+      navigation.navigate(SignInScreen)
+    } catch(err) {
+      Alert.alert(err.code);
+    }
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Hello </Text> 
+    <View style={styles.container}>
+      <Button style={{height:39}} title="Yes" onPress={signOut} color = "#884E7D"/>
     </View>
   );
-};
+}
 
-export default Profile;
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#E6E6FA',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+});
