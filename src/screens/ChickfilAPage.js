603e630e45e03d706165fb3ca7d15c1972a48267
Siona Ravi <arthiravi2524@gmail.com>
2023-02-26 20:28:47 -0500

    update rating screen button

--- src/screens/ChickfilAPage.js
+++ src/screens/ChickfilAPage.js
@@ -15,116 +15,126 @@ import HomeHeader from './HomeHeader';
 
 const ChickfilAPage = (navigation) => {
 
-const pressFoodItem = () => {
-    //move to specified restaurant screen
-}
-    return(
-            <View style = {styles.container}>
-            <Image source={ellipsepink} 
-                    style={{position: 'absolute',
-                    left:-135,
-                    top: -45,}}>
-                </Image>
-                <View style = {styles.header}>
-                <HomeHeader navigation = {navigation}/>
-                </View> 
-                <TouchableOpacity
-                  onPress={()=> navigation.naviagte(RatingScreenCFA)}>
-                <Image source={ratingstar} 
-                    style={{position: 'absolute',
-                    height:38,
-                    width: 42,
-                    left:270,
-                    bottom:16}}/>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "1. Chicken Sandwich" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "2. Spicy Chicken Sandwich" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "3. Chicken Nuggets " </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "4. Soda" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "5. Sides" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "6. Desserts" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "7. Lemonades and Sweet Teas" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress = {pressFoodItem}
-                    style = {styles.leftFoodButtons}>
-                    <Text style = {styles.buttonText}> "8. Water" </Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    onPress={() => navigation.navigate('RussellHouse')}>
-                    <Image source={leftarrow} 
-                     style={{ width: 52, 
-                    height: 50,
-                    top:160,
-                    alignSelf:'flex-start'}} />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('RatingScreenCFA')}>      
-                <Image source={ratingstar} style={{
-                    height:35,
-                    width:42 ,
-                    top:121,
-                    alignSelf:'flex-end'}}/>
-                </TouchableOpacity> 
-            </View>
+    const pressFoodItem = () => {
+        //move to specified restaurant screen
+    }
+    
+        return(
+            <SafeAreaView style = {styles.container}>
+                <View style = {styles.container}>
+                <Image source={ellipsepink} 
+                        style={{position: 'absolute',
+                        left:-135,
+                        top: -45,}}>
+                    </Image>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "1. Chicken Sandwich" </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "2. Spicy Chicken Sandwich" </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "3. Chicken Nuggets " </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "4. Soda" </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "5. Sides" </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "6. Desserts" </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "7. Lemonades and Sweet Teas" </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress = {pressFoodItem}
+                        style = {styles.leftFoodButtons}>
+                        <Text style = {styles.buttonText}> "8. Water" </Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity
+                        style = {styles.ratebtn}
+                        onPress={() => navigation.navigate('RatingScreenCFA')}>
+                        <Text style = {styles.rbtnText}> Rate us </Text>
+                    </TouchableOpacity>
+                    
+                    
+    
+                    <TouchableOpacity 
+                        onPress={() => navigation.navigate('RussellHouse')}>
+                        <Image source={leftarrow} 
+                        style={{ width: 50, 
+                        height: 50,
+                        top:-20,
+                        left:-150
+                        }} />
+                    </TouchableOpacity>
+    
+                </View>
+            </SafeAreaView>
+        )
+    }
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#B6B7E5',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    
+        leftFoodButtons:{
+            width:250,
+            backgroundColor:"#884E7D",
+            borderRadius:25,
+            height:50,
+            justifyContent:"center",
+            marginTop:10,
+            marginBottom:10
+        },
+    
+        buttonText:{
+            color:"white",
+            fontSize:20,
+            padding:2,
+            alignItems:'center',
+            justifyContent: 'center'
+            
+        },
+    
+        rbtnText:{
+            color:"white",
+            fontSize:15,
+            alignItems:'center',
+            justifyContent: 'center',
+            top:-10
+            
+        },
         
-    )
-}
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#B6B7E5',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    leftFoodButtons:{
-        width:250,
-        backgroundColor:"#884E7D",
-        borderRadius:25,
-        height:60,
-        justifyContent:"center",
-        marginTop:10,
-        marginBottom:10
-    },
-
-    buttonText:{
-        color:"white",
-        fontSize:20,
-        padding:2,
-        alignItems:'center',
-        justifyContent: 'center' 
-    },
-    header:{
-        top:-63 
-    },
-})
+        ratebtn:{
+            backgroundColor:"#884E7D",
+            justifyContent: 'flex-end',
+            left: 140,
+            width:70,
+            borderRadius:25,
+            height:50,
+            bottom:-20
+    
+        }
+    })
 export default ChickfilAPage;
