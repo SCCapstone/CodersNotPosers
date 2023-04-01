3122a3e9796e5b8d1c80cebd3a92631cda911597
Nicholas Garofalo <Garofaln@email.sc.edu>
2023-04-01 17:51:15 -0400

    finished search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -10,16 +10,31 @@ import ellipsepink from './../../images/ellipsepink.png';
 import leftarrow from './../../images/leftarrow.png';
 import ellipsegrey from './../../images/ellipsegrey.png';
 import hamburger from './../../images/hamburger.png';
-
-
-//const allRestaurants = require("../../data/AllRestaurants.json");
+import ColloquiumCafe from './EastCampus/ColloquiumCafe';
+import HorshoeDeli from './EastCampus/HorshoeDeli';
+import HumanitiesStarbucks from './EastCampus/HumanitiesStarbucks';
+import VillageJuiceAndKitchen from './EastCampus/VillageJuiceAndKitchen';
+import NachoPapis from './FoodTrucks/NachoPapis';
+import Coop from './FoodTrucks/Coop';
+import CounselorsCafe from './NorthCampus/CounselorsCafe';
+import HamptonStCafe from './NorthCampus/HamptonStCafe';
+import Creamery from './RussellHouse/Creamery';
+import ChickfilA from './RussellHouse/ChickfilA';
+import Einstein from './RussellHouse/Einstein';
+import Panda from './RussellHouse/Panda';
+import Panera from './RussellHouse/Panera';
+import TwistedTaco from './RussellHouse/TwistedTaco';
+import CafeVerde from './SouthCampus/CafeVerde';
+import WiredCafe from './SouthCampus/WiredCafe';
+import TcoopStarbs from './WestCampus/TcoopStarbs';
 
 const SearchScreen = ({navigation}) => {
 
-    var allRestaurants = [{name: "Colloquium Cafe"}, {name: "Horseshoe Deli"}, {name: "Humanities - Starbucks"}, {name: "Village Juice and Kitchen"}, {name: "Nacho Papi's"}, {name: "The Coop"},
-    {name:  "Counselor's Cafe"}, {name: "Hampton St. Cafe"}, {name: "Carolina Creamery"}, {name: "Chick-Fil-A"}, 
-    {name: "Einstein Bros. Bagels"}, {name: "Panda Express"},{ name: "Panera"}, {name: "Twisted Taco"}, {name: "Cafe Verde"},
-    {name:"Wired Cafe"}, {name: "Thomas Cooper Starbucks"}];
+    var allRestaurants = [{name: "Colloquium Cafe", screen: ColloquiumCafe}, {name: "Horseshoe Deli", screen: HorshoeDeli}, {name: "Humanities - Starbucks", screen: HumanitiesStarbucks}, 
+                            {name: "Village Juice and Kitchen", screen: VillageJuiceAndKitchen}, {name: "Nacho Papi's", screen: NachoPapis}, {name: "The Coop", screen: Coop},
+                            {name:  "Counselor's Cafe", screen: CounselorsCafe}, {name: "Hampton St. Cafe", screen: HamptonStCafe}, {name: "Carolina Creamery", screen: Creamery}, {name: "Chick-Fil-A", screen: ChickfilA}, 
+                            {name: "Einstein Bros. Bagels", screen: Einstein}, {name: "Panda Express", screen: Panda},{ name: "Panera", screen: Panera}, {name: "Twisted Taco", screen: TwistedTaco}, {name: "Cafe Verde", screen: CafeVerde},
+                            {name:"Wired Cafe", screen: WiredCafe}, {name: "Thomas Cooper Starbucks", screen: TcoopStarbs}];
 
     const [searchQuery, setSearchQuery] = useState('');
 
@@ -39,7 +54,11 @@ const SearchScreen = ({navigation}) => {
                 />
                 {filteredData.map((restaurant) => (
                     <View key={restaurant.name} style={styles.itemContainer}>
-                        <Text style={styles.itemName}>{restaurant.name}</Text>
+                        <TouchableOpacity 
+                            onPress={() => navigation.navigate(restaurant.screen)}
+                            style={styles.itemName}>
+                            <Text style={styles.loginText}>{restaurant.name}</Text>
+                        </TouchableOpacity>
                     </View>
                 ))}
                 <View style={styles.container}>
@@ -100,6 +119,7 @@ const styles = StyleSheet.create({
     },
     searchBar: {
         height: 40,
+        backgroundColor: 'white',
         borderColor: 'gray',
         borderWidth: 1,
         paddingHorizontal: 10,
@@ -114,5 +134,19 @@ const styles = StyleSheet.create({
     },
     itemDescription: {
         color: 'gray',
+    },
+    loginText:{
+        color:"white",
+        fontSize:16
+    },
+    loginBtn:{
+        width:"80%",
+        backgroundColor:"#884E7D",
+        borderRadius:25,
+        height:20,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:40,
+        marginBottom:10
     }
 })
