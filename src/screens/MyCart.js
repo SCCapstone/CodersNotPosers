75d90751f31053565d71300f22b609fc738bc87d
Nicholas Garofalo <70486605+Nicholasgarofalo@users.noreply.github.com>
2023-04-22 19:23:49 -0400

    Update MyCart.js

--- src/screens/MyCart.js
+++ src/screens/MyCart.js
@@ -14,9 +14,10 @@ const MyCart = {
       ;
     }
   },
-    clearCart: function() {
+    clearCart: function(item) {
+        const existingItems = this.items.find(i => i.item === item.item && i.price === item.price);
         while (this.items.length > 0) {
-            this.items.pop();
+            this.items.shift();
         }
     },
     removeItem: function(_item) {
