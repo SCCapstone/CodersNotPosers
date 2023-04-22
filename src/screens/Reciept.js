15b003786ba0464e45d742c7ba00df71257c32b3
Nicholas Garofalo <70486605+Nicholasgarofalo@users.noreply.github.com>
2023-04-22 18:18:30 -0400

    Update Reciept.js

--- src/screens/Reciept.js
+++ src/screens/Reciept.js
@@ -44,6 +44,11 @@ const Reciept = ({navigation}) => {
         }
       };
     
+    const returnHome = ({navigation}) => {
+        MyCart.clearCart();
+        navigation.navigate(CampusSideSelectionScreen);
+    },
+    
     // renderItem pulls the items in which the person ordered from their cart and returns that item formatted as shown below
     // NAME $PRICE QUANTITY
     const renderItem = ({ item }) => {
@@ -80,7 +85,7 @@ const Reciept = ({navigation}) => {
           </Image>
           {/*below is the code which styles the header of the screen, which has an option to take the user back to the homepage and the name of the screen on it*/}
           <View style = {styles.header}>
-              <TouchableOpacity onPress={()=>navigation.navigate(CampusSideSelectionScreen)}>
+              <TouchableOpacity onPress={()=>returnHome}>
               <Image source={home} 
               style = {{ width:35, height:35,marginRight:360, top:5 }}>
           </Image>
