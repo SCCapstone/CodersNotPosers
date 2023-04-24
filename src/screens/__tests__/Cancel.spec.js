4977d67797f7133c4c7636826ec9688e91157fff
Siona Ravi <aravi@email.sc.edu>
2023-04-24 08:45:41 -0400

    added comments

--- src/screens/__tests__/Cancel.spec.js
+++ src/screens/__tests__/Cancel.spec.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import Cancel from '../Cancel';
 
+//behavioral test
 describe('<Cancel />', () => {
   it('renders the cancel page with the correct text', () => {
     const { getByText } = render(<Cancel />);
     const title = getByText('Order Cancelled');
-    //expect(title).toBeDefined();
   });
 
   it('navigates to the CampusSideSelectionScreen on button press', () => {
@@ -14,6 +14,5 @@ describe('<Cancel />', () => {
     const { getByText } = render(<Cancel navigation={navigation} />);
     const button = getByText('Homepage');
     fireEvent.press(button);
-    //expect(navigation.navigate).toHaveBeenCalledWith('CampusSideSelectionScreen');
   });
 });
